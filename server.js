70e23124cd579f5cd1f68a3fbdd0e805b74d49f6
John Andres <johnandrescv@gmail.com>
2020-04-20 23:29:06 -0500

    Primer commit

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = express();
+const hbs = require('hbs');
+require('./hbs/helpers');
+
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/public'));
+
+// Express HBS Engine
+hbs.registerPartials(__dirname + '/views/parciales');
+app.set('view engine', 'hbs');
+
+app.get('/', (req, res) => {
+    // let salida = {
+    //     nombre: 'john',
+    //     edad: 28,
+    //     url: req.url
+    // };
+    // res.send(salida);
+
+    res.render('home', {
+        nombre: 'John Andrés',
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+app.get('/data', (req, res) => {
+    res.send('Hola Data');
+});
+
+app.listen(port, () => {
+    console.log(`Escuchando peticiones en el puerto ${port}`);
+});
